1b82ebd7903948df510bb0bf4d7d114d6c3c1378
tim <jan-brenner@gmx.de>
2017-03-11 19:59:27 +0100

    Switched to .Net 4.6.2.

--- ChatServer/Resources.Designer.cs
+++ ChatServer/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ChatClient.Properties {
+namespace Chat {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ChatClient.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ChatClient.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Chat.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
